048d6e5cce2149367f02a398935c4b72672b2593
Debasish Ghosh <ghosh.debasish@gmail.com>
2017-10-31 20:48:50 +0530

    More variants

--- code/src/main/scala/patterns/trading.scala
+++ code/src/main/scala/patterns/trading.scala
@@ -4,6 +4,12 @@ import cats._
 import cats.data._
 import cats.implicits._
 
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+/**
+ *  Plain monadic disjunction
+ */  
 trait TradingA[Account, Trade, ClientOrder, Order, Execution, Market] {
 
   def fromClientOrder: ClientOrder => Either[DomainValidation, Order]
@@ -27,6 +33,9 @@ trait TradingA[Account, Trade, ClientOrder, Order, Execution, Market] {
     clientOrders.traverse[Either[DomainValidation, ?], List[Trade]](tradeGeneration(market, broker, clientAccounts))
 }
 
+/**
+ * Monadic disjunction with ReaderT
+ */ 
 trait TradingB[Account, Trade, ClientOrder, Order, Execution, Market] {
 
   def fromClientOrder: Kleisli[Either[DomainValidation, ?], ClientOrder, Order]
@@ -40,6 +49,31 @@ trait TradingB[Account, Trade, ClientOrder, Order, Execution, Market] {
     fromClientOrder andThen execute(market, broker) andThen allocate(clientAccounts)
 
   type DomainValidationResult[A] = Either[DomainValidation, A]
+  type ReadAndValidate[A] = Kleisli[DomainValidationResult, ClientOrder, A]
+
+  def fromClientOrders(
+    market: Market, 
+    broker: Account, 
+    clientAccounts: List[Account], 
+    clientOrders: List[ClientOrder]) = clientOrders.traverse[ReadAndValidate, List[Trade]](tradeGeneration(market, broker, clientAccounts))
+}
+
+/**
+ * Stack in yet another monad - Future
+ */ 
+trait TradingC[Account, Trade, ClientOrder, Order, Execution, Market] {
+
+  def fromClientOrder: Kleisli[EitherT[Future, DomainValidation, ?], ClientOrder, Order]
+  def execute(market: Market, brokerAccount: Account): Kleisli[EitherT[Future, DomainValidation, ?], Order, List[Execution]]
+  def allocate(accounts: List[Account]): Kleisli[EitherT[Future, DomainValidation, ?], List[Execution], List[Trade]]
+
+  def tradeGeneration(
+    market: Market, 
+    broker: Account, 
+    clientAccounts: List[Account])(clientOrder: ClientOrder): Kleisli[EitherT[Future, DomainValidation, ?], ClientOrder, List[Trade]] = 
+    fromClientOrder andThen execute(market, broker) andThen allocate(clientAccounts)
+
+  type DomainValidationResult[A] = EitherT[Future, DomainValidation, A]
   type ReadAndValidate[A] = Kleisli[DomainValidationResult, ClientOrder, A]
 
   def fromClientOrders(
