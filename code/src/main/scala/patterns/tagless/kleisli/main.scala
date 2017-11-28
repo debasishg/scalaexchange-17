0973452f64da1bf5a8fd4440af30a37b4f6ae27b
Debasish Ghosh <ghosh.debasish@gmail.com>
2017-11-29 00:28:42 +0530

    Minor refactoring of the Kleisli part

--- code/src/main/scala/patterns/tagless/kleisli/main.scala
+++ code/src/main/scala/patterns/tagless/kleisli/main.scala
@@ -11,29 +11,31 @@ import TradeModel._
 
 object Main {
 
-  val m1 = makeMarket("Tokyo")
-  val m2 = makeMarket("NewYork")
-  val m3 = makeMarket("Singapore")
+  trait Data {
+    val m1 = makeMarket("Tokyo")
+    val m2 = makeMarket("NewYork")
+    val m3 = makeMarket("Singapore")
 
-  val ba = "broker-account-1"
+    val ba = "broker-account-1"
 
-  val ca1 = "client-account-1"
-  val ca2 = "client-account-2"
-  val ca3 = "client-account-3"
+    val ca1 = "client-account-1"
+    val ca2 = "client-account-2"
+    val ca3 = "client-account-3"
 
-  val cor = Map(
-    "no" -> "client-order-1",
-    "customer" -> "customer-1",
-    "instrument" -> "ibm/100/1000-google/200/2000"
-  )
+    val cor = Map(
+      "no" -> "client-order-1",
+      "customer" -> "customer-1",
+      "instrument" -> "ibm/100/1000-google/200/2000"
+    )
+  }
 
-  object TradingComponent extends TradingInterpreter[IO]
+  object TradeGenerationIO extends Data {
+    def tradeGeneration[M[_]: FlatMap](T: Trading[M]): Kleisli[M, ClientOrder, List[Trade]] = 
+      T.fromClientOrder andThen T.execute(m1, ba) andThen T.allocate(List(ca1, ca2, ca3))
 
-  def tradeGeneration[F[_]](T: Trading[F])
-    (implicit me: MonadError[F, Throwable]): Kleisli[F, ClientOrder, List[Trade]] = 
-      T.fromClientOrder andThen T.execute(m2, ba) andThen T.allocate(List(ca1, ca2, ca3))
+    object TradingComponent extends TradingInterpreter[IO]
+    val tk = tradeGeneration(TradingComponent)
 
-  val tk = tradeGeneration(TradingComponent)
-  tk(cor).unsafeRunSync
+    tk(cor).unsafeRunSync
+  }
 }
-
